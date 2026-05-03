0c420c34a8a232bb7a1629022fd525a505f29251
Will Wolf <willwolf@Wills-Air-6.stdusr.yale.internal>
2026-05-03 03:24:52 -0400

    will stuf

--- GSE/gse_control/procedures.cpp
+++ GSE/gse_control/procedures.cpp
@@ -79,7 +79,7 @@ void coldFlowEthane(long duration_ms) {
   EthaneMBV->setNextActuation(RUN_EQUALIZE_TIME);
   EthaneMBV->setNextActuation(RUN_EQUALIZE_TIME + duration_ms);
 
-  EthaneRunValve.setNextActuation(1000 + duration_ms, false);
+  EthaneRunValve.setNextActuation(RUN_EQUALIZE_TIME + duration_ms, false);
 }
 
 void coldFlowNitrous(long duration_ms) {
@@ -88,7 +88,7 @@ void coldFlowNitrous(long duration_ms) {
   NitrousMBV->setNextActuation(RUN_EQUALIZE_TIME);
   NitrousMBV->setNextActuation(RUN_EQUALIZE_TIME + duration_ms);
 
-  NitrousRunValve.setNextActuation(2*RUN_EQUALIZE_TIME + duration_ms, false);
+  NitrousRunValve.setNextActuation(RUN_EQUALIZE_TIME + duration_ms, false);
 }
 
 void staticFire() {
